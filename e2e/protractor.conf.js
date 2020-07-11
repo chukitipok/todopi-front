1a403dbe545ea059df6761d7fff47e6b6c5d17f0
NenetteGit <antonin.texier@gmail.com>
2020-07-11 17:02:03 +0200

    fix(e2e): Try to fix CI workflow.

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -24,7 +24,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 2000,
+    defaultTimeoutInterval: 1000000,
     print: function() {}
   },
   onPrepare() {
