066a8f8d34d57030ccbf340b3372821974542d02
Simon Halimi <halimisimon@gmail.com>
2020-07-15 16:33:40 +0200

    feat(login): Add test on login enable

--- e2e/src/app/login-scenario/login.e2e-spec.ts
+++ e2e/src/app/login-scenario/login.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { LoginPage } from './login.po';
-import {browser, logging} from 'protractor';
+import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Login full scenario', () => {
   let page: LoginPage;
 
   beforeEach(() => {
@@ -16,6 +16,32 @@ describe('workspace-project App', () => {
     page.getSubmitButton().click();
 
     expect(await page.getCurrentUrl()).toBe(browser.baseUrl + 'login');
+  });
+
+
+  describe('Login Button clickable', () => {
+    it('should not enable Login button if email is not filled', async () => {
+      await page.navigateTo();
+
+      page.getPassword().sendKeys('FakePassword');
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+
+    it('should not enable Login button if password is not filled', async () => {
+      await page.navigateTo();
+
+      page.getEmail().sendKeys('toto@toto.fr');
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+
+    it('should enable Login button if both field are filled', async () => {
+      await page.navigateTo();
+
+      page.getEmail().sendKeys('toto@toto.fr');
+      page.getPassword().sendKeys('toto@toto.fr');
+
+      expect(page.getSubmitButton().isEnabled()).toBe(true);
+    });
   });
 
   afterEach(async () => {
