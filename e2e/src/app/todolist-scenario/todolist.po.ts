e9a54cf13c4cba2391ed8d46a266dcce9a2b3212
Simon Halimi <halimisimon@gmail.com>
2020-07-22 15:58:05 +0200

    feat(): Create some tests

--- e2e/src/app/todolist-scenario/todolist.po.ts
+++ e2e/src/app/todolist-scenario/todolist.po.ts
@@ -5,5 +5,27 @@ export class TodolistPage {
         return browser.get(browser.baseUrl + '/todolist') as Promise<unknown>;
     }
 
-    
+    getCurrentUrl(): Promise<string> {
+        return browser.getCurrentUrl() as Promise<string>;
+    }
+
+    getNewTodolist(): ElementFinder {
+        return element(by.id('list-name'));
+    }
+
+    getTodolistCreationInput(): ElementFinder {
+        return element(by.id('list-name'));
+    }
+
+    getTodolistSubmitButton(): ElementFinder {
+        return element(by.id('submit-todolist'));
+    }
+
+    async getAllNewTodolist(): Promise<ElementArrayFinder> {
+        return await element.all(by.id('new-todolist'));
+    }
+
+    async getAllNewTodolistContent(): Promise<ElementArrayFinder> {
+        return await element.all(by.id('todolist-content'));
+    }
 }
